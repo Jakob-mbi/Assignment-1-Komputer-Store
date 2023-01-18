9bbc7fe66c379b824e264bdbf7d5b0eeeffcc6e4
Jakob-mbi <jakob_mbi@msn.com>
2023-01-18 19:15:34 +0100

    Final draft

--- Assignment 1 Komputer Store/JS/App.js
+++ Assignment 1 Komputer Store/JS/App.js
@@ -1,6 +1,5 @@
 const bankBalance = document.getElementById('bankBalance');
 const loanBalance = document.getElementById('loanBalance');
-const borrowElement = document.getElementById('borrowBtn');
 const changeVisibility = document.getElementById('loan');
 const changeVisibilityWork = document.getElementById('Repay');
 const payBalance = document.getElementById('payBalance');
@@ -18,6 +17,8 @@ let currentPay = 0;
 bankBalance.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(currentBalance)}`;
 payBalance.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(currentPay)}`;
 
+//Figure 3 Laptop Selection Area & Figure 4 Laptop Information Area
+
 fetch("https://hickory-quilled-actress.glitch.me/computers")
     .then(response => response.json())
     .then(data => computers = data)
@@ -25,15 +26,15 @@ fetch("https://hickory-quilled-actress.glitch.me/computers")
 
 const addComputersToSelector = (computers) => {
     computers.map(element => addComputerToSelector(element));
-    featuresElement.innerText = computers[0].specs.join('.\r');
-    priceElement.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(computers[0].price)}`;
+    featuresElement.innerText = computers[0].specs.join('.\r');    
     descriptionElement.innerText = computers[0].description;
     productElement.innerText = computers[0].title;
     imgElement.src = `https://hickory-quilled-actress.glitch.me/${computers[0].image}`
+    updateUiCurrencyAmount(priceElement,computers[0].price);
 }
 
 
-const addComputerToSelector = (computer) => {
+const addComputerToSelector = computer => {
     const computerElement = document.createElement("option");
     computerElement.value = computer.id;
     computerElement.appendChild(document.createTextNode(computer.title));
@@ -42,69 +43,61 @@ const addComputerToSelector = (computer) => {
 const handleComputerSelectChange = e => {
     const selctedComputer = computers[e.target.selectedIndex];
     featuresElement.innerText = selctedComputer.specs.join('.\r');
-    priceElement.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(selctedComputer.price)}`;
     descriptionElement.innerText = selctedComputer.description;
     productElement.innerText = selctedComputer.title;
     imgElement.src = `https://hickory-quilled-actress.glitch.me/${selctedComputer.image}`
+    updateUiCurrencyAmount(priceElement,selctedComputer.price);
 }
-const buy = () =>{
+
+const buy = () => {
     const selctedComputer = computers[computersElement.selectedIndex];
-    withdrawal(selctedComputer.price)
+    if((withdrawal(selctedComputer.price))){
+        alert(`You are now the owner of the new ${selctedComputer.title}`)
+    }
 }
+
+//Figure 1 The Bank Section
+
 //Bank Balance 
 
 const withdrawal = function(withdrawalAmount) {
-    if(withdrawalAmount <= currentBalance){
-        currentBalance -= withdrawalAmount;
-        bankBalance.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(currentBalance)}`;
-        return true;
+    if(withdrawalAmount > currentBalance){
+        return alert('Your current bank balance is to low!')   
     }
-    else{
-        alert('Your current bank balance is to low!')
-        return false;
-    }
+    currentBalance -= withdrawalAmount;
+    updateUiCurrencyAmount(bankBalance,currentBalance);
+    return true
 }
 const deposit = function(depositAmount){
-    Number(depositAmount);
     currentBalance += depositAmount;
-    bankBalance.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(currentBalance)}`;
+    updateUiCurrencyAmount(bankBalance,currentBalance);   
 }
-
 
 //Loan Balance 
 
 const borrow = function(){
-    if(currentDebt <= 0)
+    if(currentDebt != 0)
     {
-        const borrowAmount = Number(prompt('Please enter the amount you want to borrow')); 
-        const maxBorrowAmount = currentBalance*2;
-        if(borrowAmount>maxBorrowAmount){
-            alert('You cannot get a loan more than double your current bank balance');
-        } 
-        else{
-            currentDebt = borrowAmount;
-            currentBalance += currentDebt;
-            changeVisibility.style.visibility = "visible";
-            changeVisibilityWork.style.visibility = "visible";
-        }
+        return alert('You need to pay back current debt in full to get a new loan')
     }
-    else
-    {
-        alert('You need to pay back current debt in full to get a new loan')
-    }
-    loanBalance.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(currentDebt)}`
-    bankBalance.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(currentBalance)}`;
+    const borrowAmount = Number(prompt('Please enter the amount you want to borrow')); 
+    const maxBorrowAmount = currentBalance*2;
+    if(borrowAmount>maxBorrowAmount){
+        return alert('You cannot get a loan more than double your current bank balance');
+    } 
+    currentDebt = borrowAmount;
+    currentBalance += currentDebt;
+    showAndHidde(true) 
+    updateUiCurrencyAmount(loanBalance,currentDebt);
+    updateUiCurrencyAmount(bankBalance,currentBalance);
 }
 const repayLoan = function (number){
-    currentDebt -= Number(number); 
-    if(currentDebt <= 0)
-    {
-        changeVisibility.style.visibility = "hidden"
-        changeVisibilityWork.style.visibility = "hidden"
-    }
-
-    loanBalance.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(currentDebt)}`
+    currentDebt -= number; 
+    if(currentDebt <= 0){showAndHidde(false)}
+    updateUiCurrencyAmount(loanBalance,currentDebt);
 }
+
+// Figure 2 Work Section
 
 //Pay Balance 
 
@@ -125,14 +118,14 @@ const bankTransfer = function(){
         deposit(currentPay);   
     }
     currentPay = 0;
-    payBalance.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(currentPay)}`;
+    updateUiCurrencyAmount(payBalance,currentPay);
 }
 const working = function(){
     currentPay += 100;
-    payBalance.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(currentPay)}`;
+    updateUiCurrencyAmount(payBalance,currentPay);
 }
 
-const repayPay = function(){
+const repayFromPay = function(){
     if(currentPay<currentDebt)
     {
         repayLoan(currentPay);
@@ -144,13 +137,23 @@ const repayPay = function(){
         repayLoan(currentDebt)
     }
     currentPay=0;
-    payBalance.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(currentPay)}`
+    updateUiCurrencyAmount(payBalance,currentPay);
 }
 
+// Repeated code
+
+const showAndHidde = e => {
+    e ? changeVisibility.style.visibility = "visible" : changeVisibility.style.visibility = "hidden";
+    e ? changeVisibilityWork.style.visibility = "visible" : changeVisibilityWork.style.visibility = "hidden"; 
+}
+const currencyFormat = e => {return `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(e)}`};
+const updateUiCurrencyAmount= (i,c) => {return i.innerText = currencyFormat(c)};
+
+//Event Listners
 
 document.getElementById("borrowBtn").addEventListener("click",borrow);
 document.getElementById("work").addEventListener("click",working);
-document.getElementById("Repay").addEventListener("click",repayPay);
+document.getElementById("Repay").addEventListener("click",repayFromPay);
 document.getElementById("bankTransfer").addEventListener("click",bankTransfer);
 document.getElementById('computerSelection').addEventListener("change",handleComputerSelectChange);
 document.getElementById('buyBtn').addEventListener("click",buy);
