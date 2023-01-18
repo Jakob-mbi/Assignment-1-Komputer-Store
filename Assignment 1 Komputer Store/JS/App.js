5067a6d271e185690c724b60d678a36bc6e1fef1
Jakob-mbi <jakob_mbi@msn.com>
2023-01-18 17:28:30 +0100

    first draft draft fully working

--- Assignment 1 Komputer Store/JS/App.js
+++ Assignment 1 Komputer Store/JS/App.js
@@ -5,9 +5,18 @@ const changeVisibility = document.getElementById('loan');
 const changeVisibilityWork = document.getElementById('Repay');
 const payBalance = document.getElementById('payBalance');
 const featuresElement = document.getElementById('features')
-const computersElement = document.getElementById('laptops');
+const computersElement = document.getElementById('computerSelection');
+const productElement = document.getElementById('product')
+const descriptionElement = document.getElementById('description')
+const priceElement = document.getElementById('price')
+const imgElement = document.getElementById('img');
 
 let computers = [];
+let currentBalance = 0;
+let currentDebt = 0;
+let currentPay = 0;
+bankBalance.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(currentBalance)}`;
+payBalance.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(currentPay)}`;
 
 fetch("https://hickory-quilled-actress.glitch.me/computers")
     .then(response => response.json())
@@ -16,6 +25,11 @@ fetch("https://hickory-quilled-actress.glitch.me/computers")
 
 const addComputersToSelector = (computers) => {
     computers.map(element => addComputerToSelector(element));
+    featuresElement.innerText = computers[0].specs.join('.\r');
+    priceElement.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(computers[0].price)}`;
+    descriptionElement.innerText = computers[0].description;
+    productElement.innerText = computers[0].title;
+    imgElement.src = `https://hickory-quilled-actress.glitch.me/${computers[0].image}`
 }
 
 
@@ -25,13 +39,18 @@ const addComputerToSelector = (computer) => {
     computerElement.appendChild(document.createTextNode(computer.title));
     computersElement.appendChild(computerElement);
 }
-
-let currentBalance = 0;
-let currentDebt = 0;
-let currentPay = 0;
-bankBalance.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(currentBalance)}`;
-payBalance.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(currentPay)}`;
-
+const handleComputerSelectChange = e => {
+    const selctedComputer = computers[e.target.selectedIndex];
+    featuresElement.innerText = selctedComputer.specs.join('.\r');
+    priceElement.innerText = `${new Intl.NumberFormat('sv',{style: 'currency',currency: 'SEK'}).format(selctedComputer.price)}`;
+    descriptionElement.innerText = selctedComputer.description;
+    productElement.innerText = selctedComputer.title;
+    imgElement.src = `https://hickory-quilled-actress.glitch.me/${selctedComputer.image}`
+}
+const buy = () =>{
+    const selctedComputer = computers[computersElement.selectedIndex];
+    withdrawal(selctedComputer.price)
+}
 //Bank Balance 
 
 const withdrawal = function(withdrawalAmount) {
@@ -129,10 +148,12 @@ const repayPay = function(){
 }
 
 
-const getALoan = document.getElementById("borrowBtn").addEventListener("click",borrow);
-const work = document.getElementById("work").addEventListener("click",working);
-const repay = document.getElementById("Repay").addEventListener("click",repayPay);
-const transfer = document.getElementById("bankTransfer").addEventListener("click",bankTransfer);
+document.getElementById("borrowBtn").addEventListener("click",borrow);
+document.getElementById("work").addEventListener("click",working);
+document.getElementById("Repay").addEventListener("click",repayPay);
+document.getElementById("bankTransfer").addEventListener("click",bankTransfer);
+document.getElementById('computerSelection').addEventListener("change",handleComputerSelectChange);
+document.getElementById('buyBtn').addEventListener("click",buy);
 
 
 
